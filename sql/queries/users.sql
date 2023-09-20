24ae148f974fe33ef51d2af2094edb48e2388a71
dannyhkim <dannykim200@gmail.com>
2023-09-19 22:05:22 -0400

    adding handlers for users, feeds, feed follows, posts

---
+++ sql/queries/users.sql
@@ -0,0 +1,21 @@
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, name, api_key)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    encode(sha256(random()::text::bytea), 'hex')
+  )
+RETURNING *;
+
+-- name: GetUserByAPIKey :one
+SELECT * FROM users WHERE api_key = $1;
+
+
+-- name: GetPostsForUser :many
+SELECT posts.* from posts
+JOIN feed_follows ON posts.feed_id = feed_follows.feed_id
+WHERE feed_follows.user_id = $1
+ORDER BY posts.published_at DESC
+LIMIT $2;
